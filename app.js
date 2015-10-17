6d3ea69c20329d25139a402729ce7d6e0b16bbf8
ingridtartaglia <ingrid.tartaglia@gmail.com>
2015-10-17 11:20:11 -0300

    ng-click

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var colorPickerApp = angular.module('colorPickerApp', ['mp.colorPicker']);
 
 colorPickerApp.controller('colorPickerCtrl', function($scope){
-
+    $scope.saveColor = function(color) {
+        
+    }
 });
