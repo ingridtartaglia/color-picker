88a9e3af9f6e25f297ac68fae6ac8c82c6cd7e51
Ingrid Tartaglia <ingrid.tartaglia@gmail.com>
2015-11-17 15:43:34 -0200

    styling

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var colorPickerApp = angular.module('colorPickerApp', ['mp.colorPicker']);
 
 colorPickerApp.controller('colorPickerCtrl', function($scope){
+    $scope.colorPicked = false;
+    $scope.newColors = [];
+
     $scope.saveColor = function(color) {
-        
+        $scope.colorPicked = true;
+        $scope.newColors.push(color);
     }
 });
